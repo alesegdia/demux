ca3f89623f6c8708a5e2f80863abd253af90eec6
Alejandro Seguí <alesegdia@gmail.com>
2015-12-31 22:22:54 +0100

    using actual layout generator at game

--- game/core/src/com/alesegdia/demux/GdxGame.java
+++ game/core/src/com/alesegdia/demux/GdxGame.java
@@ -1,11 +1,26 @@
 package com.alesegdia.demux;
 
+import java.util.List;
+
 import com.alesegdia.demux.assets.Tmx;
 import com.alesegdia.demux.assets.TmxRoomInfoLoader;
 import com.alesegdia.demux.screen.GameScreen;
+import com.alesegdia.troidgen.GraphBuilder;
+import com.alesegdia.troidgen.IRoomProvider;
+import com.alesegdia.troidgen.LayoutBuilder;
+import com.alesegdia.troidgen.LayoutBuilderConfig;
+import com.alesegdia.troidgen.LinkBuilder;
 import com.alesegdia.troidgen.ManualRoomProvider;
+import com.alesegdia.troidgen.MinSizeRoomGroupValidator;
+import com.alesegdia.troidgen.OverlapSolverConfig;
+import com.alesegdia.troidgen.RandomRoomProvider;
+import com.alesegdia.troidgen.renderer.RectDebugger;
+import com.alesegdia.troidgen.restriction.RestrictionSet;
 import com.alesegdia.troidgen.room.Room;
 import com.alesegdia.troidgen.room.RoomInfo;
+import com.alesegdia.troidgen.room.RoomType;
+import com.alesegdia.troidgen.util.Rect;
+import com.alesegdia.troidgen.util.UpperMatrix2D;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.OrthographicCamera;
@@ -30,7 +45,38 @@ public class GdxGame extends Game {
         Tmx.Initialize();
         
         ManualRoomProvider mrp = new ManualRoomProvider();
-        mrp.addGeometryModel(Tmx.GetMap("tpl_common").createRoom());
+        mrp.addAll(Tmx.GetRoomsOfType(RoomType.COMMON));
+        
+		LayoutBuilder lb = new LayoutBuilder();
+		LayoutBuilderConfig lbc = new LayoutBuilderConfig();
+		lbc.spawnRect = new Rect(-10, -10, 20, 20);
+		lbc.numIterations = 20;
+		
+		OverlapSolverConfig osc = new OverlapSolverConfig();
+		osc.separationParameter = 1f;
+		osc.enableTweakNearSeparation = false;
+		osc.resolution = 64;
+		osc.enclosingRect = new Rect(-20, -15, 40, 30);
+		
+		lbc.osc = osc;
+		lbc.spawnRect = new Rect(-8, -8, 16, 16);
+		
+		MinSizeRoomGroupValidator msrge = new MinSizeRoomGroupValidator( 20 );
+
+		RestrictionSet rs = new RestrictionSet(4, true, true, true, true);
+		List<Room> result = lb.generate(lbc, mrp, msrge, rs);
+		GraphBuilder gb = new GraphBuilder();
+		UpperMatrix2D<Float> m = gb.build(result);
+		System.out.println(m);
+		
+		System.out.println(result);
+		
+		LinkBuilder linksb = new LinkBuilder();
+		linksb.generate(result);
+
+		RectDebugger rd = new RectDebugger(result, 800, 600, osc.enclosingRect);
+		rd.Show();
+
         
         gameScreen = new GameScreen(this);
         setScreen(gameScreen);
