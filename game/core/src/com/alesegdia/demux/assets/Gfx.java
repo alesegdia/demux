120ae09815e5764cc2065f3a4ccdc4d42adbd021
Alejandro Seguí <alesegdia@gmail.com>
2016-01-03 05:43:59 +0100

    adding base Gfx file

---
+++ game/core/src/com/alesegdia/demux/assets/Gfx.java
@@ -0,0 +1,13 @@
+package com.alesegdia.demux.assets;
+
+import com.badlogic.gdx.graphics.g2d.Animation;
+
+public class Gfx {
+
+	public static Animation playerWalk;
+	public static Animation playerStand;
+	public static Animation playerJumpUp;
+	public static Animation playerJumpDown;
+	public static Animation playerFly;
+
+}
