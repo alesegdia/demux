d2ece71fa575b92ef70d31388861d312e9b1bb71
Alejandro Seguí <alesegdia@gmail.com>
2016-01-03 21:59:06 +0100

    tweaking GameConfig

--- game/core/src/com/alesegdia/demux/GameConfig.java
+++ game/core/src/com/alesegdia/demux/GameConfig.java
@@ -2,11 +2,11 @@ package com.alesegdia.demux;
 
 public class GameConfig {
 
-	public static final int WINDOW_WIDTH = 480;
-	public static final int WINDOW_HEIGHT = 480;
+	public static final int WINDOW_WIDTH = 800;
+	public static final int WINDOW_HEIGHT = 600;
 	
 	public static final float RATIO = ((float)WINDOW_WIDTH) / ((float)WINDOW_HEIGHT);
-	public static final float VIEWPORT_WIDTH = 10.f;
+	public static final float VIEWPORT_WIDTH = 14.f;
 	public static final float VIEWPORT_HEIGHT = VIEWPORT_WIDTH / RATIO;
 	
 	public static final float METERS_TO_PIXELS = 16f;
