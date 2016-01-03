1df53101cb4de6caa423589d894a7c77c63b83ca
Alejandro Seguí <alesegdia@gmail.com>
2016-01-03 05:44:57 +0100

    adding physics from asroth

---
+++ game/core/src/com/alesegdia/demux/physics/CollisionLayers.java
@@ -0,0 +1,67 @@
+package com.alesegdia.demux.physics;
+
+public class CollisionLayers {
+
+	public static final short CATEGORY_MAP 			= 0x0001;
+	public static final short GROUP_MAP 			= -1;
+	
+	public static final short CATEGORY_PLAYERPHYSIC	= 0x0002;
+	public static final short GROUP_PLAYERPHYSIC	= -2;
+	
+	public static final short CATEGORY_PLAYERLOGIC	= 0x0004;
+	public static final short GROUP_PLAYERLOGIC 	= -3;
+	
+	public static final short CATEGORY_PLBULLETS 	= 0x0008;
+	public static final short GROUP_PLBULLETS 		= -4;
+	
+	public static final short CATEGORY_ENEMYPHYSIC 	= 0x0010;
+	public static final short GROUP_ENEMYPHYSIC 	= -5;
+	
+	public static final short CATEGORY_ENEMYLOGIC 	= 0x0020;
+	public static final short GROUP_ENEMYLOGIC 		= -6;
+	
+	public static final short CATEGORY_ENBULLETS 	= 0x0040;
+	public static final short GROUP_ENBULLETS 		= -7;
+	
+	public static final short CATEGORY_1WAYPLATS 	= 0x0080;
+	public static final short GROUP_1WAYPLATS 		= -8;
+	
+	public static final short CATEGORY_ENEMYLIMIT	= 0x0100;
+	public static final short GROUP_ENEMYLIMIT 		= -9;
+	
+	public static final short CATEGORY_PICKUP		= 0x0200;
+	public static final short GROUP_PICKUP 			= -10;
+
+	public static final short CATEGORY_SHOP			= 0x0400;
+	public static final short GROUP_SHOP 			= -11;
+
+	public static final short CATEGORY_PORTAL		= 0x0800;
+	public static final short GROUP_PORTAL 			= -12;
+
+	/* MAP PHYSICS *************************************************************/
+	public static final short MASK_MAP =
+			CATEGORY_PLAYERPHYSIC 	| CATEGORY_PLBULLETS 	|
+			CATEGORY_ENBULLETS 		| CATEGORY_ENEMYPHYSIC;
+	public static final short MASK_1WAYPLATS = CATEGORY_PLAYERPHYSIC;
+	public static final short MASK_ENEMYLIMIT = CATEGORY_ENEMYPHYSIC | CATEGORY_ENEMYLOGIC;
+
+	/* PLAYER PHYSICS *************************************************************/
+	public static final short MASK_PLAYERPHYSIC = CATEGORY_ENBULLETS | CATEGORY_1WAYPLATS | CATEGORY_MAP;
+	public static final short MASK_PLAYERLOGIC = CATEGORY_ENEMYLOGIC | CATEGORY_ENBULLETS | CATEGORY_PICKUP | CATEGORY_SHOP | CATEGORY_PORTAL;
+	public static final short MASK_PLBULLETS = CATEGORY_ENEMYLOGIC | CATEGORY_MAP | CATEGORY_SHOP;
+
+	/* ENEMY PHYSICS *************************************************************/
+	public static final short MASK_ENEMYPHYSIC = CATEGORY_MAP;
+	public static final short MASK_ENEMYLOGIC = CATEGORY_PLAYERLOGIC | CATEGORY_PLBULLETS | CATEGORY_ENEMYLIMIT;
+	public static final short MASK_ENBULLETS = CATEGORY_PLAYERLOGIC | CATEGORY_MAP;
+	
+	/* PICKUPS *******************************************************************/
+	public static final short MASK_PICKUP = CATEGORY_PLAYERLOGIC;
+	
+	/* SHOPKEEPER ****************************************************************/
+	public static final short MASK_SHOP = CATEGORY_PLAYERLOGIC | CATEGORY_PLBULLETS;
+	
+	public static final short MASK_PORTAL = CATEGORY_PLAYERLOGIC;
+	
+
+}
---
+++ game/core/src/com/alesegdia/demux/physics/Physics.java
@@ -0,0 +1,195 @@
+package com.alesegdia.demux.physics;
+
+import com.alesegdia.demux.GameConfig;
+import com.alesegdia.demux.systems.PhysicsSystem;
+import com.badlogic.gdx.graphics.Camera;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.CircleShape;
+import com.badlogic.gdx.physics.box2d.Fixture;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.Shape;
+import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.utils.Array;
+
+public class Physics {
+
+	World world;
+	Box2DDebugRenderer debugRenderer;
+	private float accumulator = 0;
+	private static final float TIME_STEP = 1/60.f;
+	private static final int VELOCITY_ITERATIONS = 6;
+	private static final int POSITION_ITERATIONS = 2;
+	
+	
+	public PhysicsSystem physicsSystem;
+	
+	public Physics() {
+		world = new World(new Vector2(0, -10f), true);
+		physicsSystem = new PhysicsSystem();
+		world.setContactListener(this.physicsSystem);
+		debugRenderer = new Box2DDebugRenderer();
+	}
+	
+	public void step(float deltaTime) {
+
+	    accumulator += deltaTime;
+	    while (accumulator >= TIME_STEP) {
+	        world.step(TIME_STEP, VELOCITY_ITERATIONS, POSITION_ITERATIONS);
+	        accumulator -= TIME_STEP;
+	    }
+	    //world.step(TIME_STEP, VELOCITY_ITERATIONS, POSITION_ITERATIONS);
+	}
+	
+	public void render(Camera cam) {
+		debugRenderer.render(world, cam.combined);
+	}
+	
+	Body createBody(float x, float y, boolean dynamic) {
+		BodyDef bodyDef = new BodyDef();
+		if( dynamic ) {
+			bodyDef.type = BodyType.DynamicBody;
+		} else {
+			bodyDef.type = BodyType.StaticBody;
+		}
+		bodyDef.position.set(x, y);
+		Body b = world.createBody(bodyDef);
+		b.setFixedRotation(true);
+		return b;
+	}
+	
+	public Fixture createFixture(Body body, Shape shape, short catbits, short maskbits, short group, float density, float friction, float restitution ) {
+		return createFixture(body, shape, catbits, maskbits, group, density, friction, restitution, false);
+	}
+	
+	public Fixture createFixture(Body body, Shape shape, short catbits, short maskbits, short group, float density, float friction,
+			float restitution, boolean isSensor ) {
+		FixtureDef fd = new FixtureDef();
+		fd.shape = shape;
+		fd.density = density;
+		fd.friction = friction;
+		fd.restitution = restitution;
+		fd.filter.categoryBits = catbits;
+		fd.filter.maskBits = maskbits;
+		fd.filter.groupIndex = group;
+		fd.isSensor = isSensor;
+		return body.createFixture(fd);
+	}
+	
+	public Body createCircleBody(float x, float y, float radius, short catbits, short maskbits, short group, boolean dynamic) {
+		Body b = createBody(x * GameConfig.PIXELS_TO_METERS, y* GameConfig.PIXELS_TO_METERS,dynamic);
+		CircleShape cs = new CircleShape();
+		cs.setRadius(radius * GameConfig.PIXELS_TO_METERS);
+		createFixture(b, cs, catbits, maskbits, group, 1f, 0f, 0f);
+		cs.dispose();
+		return b;
+	}
+	
+	public Body createRectBody(float x, float y, float w, float h, short cat, short mask, short group, boolean dyn) {
+		return createRectBody(x,y,w,h,cat,mask,group,dyn,false);		
+	}
+	
+	public Body createRectBody(float x, float y, float w, float h, short catbits, short maskbits, short group, boolean dynamic, boolean isSensor) {
+		Body b = createBody(x,y,dynamic);
+		PolygonShape ps = new PolygonShape();
+		ps.setAsBox(w, h);
+		createFixture(b, ps, catbits, maskbits, group, 0, 0, 0, isSensor);
+		ps.dispose();
+		return b;
+	}
+	
+	public Body createEnemyBody(float x, float y) {
+		return createCircleBody(x, y, 10, CollisionLayers.CATEGORY_ENEMYPHYSIC, CollisionLayers.MASK_ENEMYPHYSIC, CollisionLayers.GROUP_ENEMYPHYSIC, true);
+	}
+
+	public Body createPlayerBody(float x, float y) {
+		Body b = createBody(x * GameConfig.PIXELS_TO_METERS, y* GameConfig.PIXELS_TO_METERS, true);
+		CircleShape cs = new CircleShape();
+		cs.setRadius(7.41f * GameConfig.PIXELS_TO_METERS);
+		createFixture(b, cs, CollisionLayers.CATEGORY_PLAYERPHYSIC, CollisionLayers.MASK_PLAYERPHYSIC, CollisionLayers.GROUP_PLAYERPHYSIC, 1f, 0f, 0f);
+		createFixture(b, cs, CollisionLayers.CATEGORY_PLAYERLOGIC, CollisionLayers.MASK_PLAYERLOGIC, CollisionLayers.GROUP_PLAYERPHYSIC, 1f, 0f, 0f);
+		cs.dispose();
+		return b;
+
+	}
+	
+	public Body createBulletBody( float x, float y, float w, float h, short cat, short mask, short group ) {
+		return createDirectionalBullet(x,y,w,h,0,cat,mask,group);
+	}
+	
+	public Body createDirectionalBullet( float x, float y, float w, float h, float angle, short cat, short mask, short group ) {
+		Body b = createBody(x, y, true);
+		b.setBullet(true);
+		CircleShape cs = new CircleShape();
+		cs.setRadius(w * GameConfig.PIXELS_TO_METERS);
+		/*
+		PolygonShape ps = new PolygonShape();
+		ps.setAsBox(w * GameConfig.PIXELS_TO_METERS, h * GameConfig.PIXELS_TO_METERS);
+		*/
+		createFixture(b, cs, cat, mask, group, 0, 0, 0, true);
+		b.setGravityScale(0);
+		cs.dispose();
+		return b;
+	}
+	
+	public Body createPlayerBulletBody( float x, float y ) {
+		return createBulletBody(x, y, 5, 5, CollisionLayers.CATEGORY_PLBULLETS, CollisionLayers.MASK_PLBULLETS, CollisionLayers.GROUP_PLBULLETS);
+	}
+
+	public Body createGroundExplosionBody( float x, float y ) {
+		Body b = createBody(x, y, true);
+		PolygonShape ps = new PolygonShape();
+		ps.setAsBox(5 * GameConfig.PIXELS_TO_METERS, 5 * GameConfig.PIXELS_TO_METERS);
+		createFixture(b, ps, CollisionLayers.CATEGORY_PLBULLETS, CollisionLayers.MASK_PLBULLETS, CollisionLayers.MASK_PLBULLETS, 0, 0, 0, true);
+		b.setGravityScale(0);
+		ps.dispose();
+		return b;
+	}
+	
+	public Body createEnemyBody2( float x, float y, float w, float h ) {
+		Body b = createBody(x, y, true);
+		PolygonShape ps = new PolygonShape();
+		ps.setAsBox(w * GameConfig.PIXELS_TO_METERS, h * GameConfig.PIXELS_TO_METERS);
+		createFixture(b, ps, CollisionLayers.CATEGORY_ENEMYPHYSIC, CollisionLayers.MASK_ENEMYPHYSIC, CollisionLayers.GROUP_ENEMYPHYSIC, 1f, 0, 0, false);
+		createFixture(b, ps, CollisionLayers.CATEGORY_ENEMYLOGIC, CollisionLayers.MASK_ENEMYLOGIC, CollisionLayers.GROUP_ENEMYPHYSIC, 0.01f, 0, 0, true);
+		ps.dispose();
+		return b;
+	}
+
+	public Body createPickupBody(float x, float y, float s) {
+		Body b = createBody(x, y, true);
+		b.setGravityScale(0);
+		CircleShape cs = new CircleShape();
+		cs.setRadius(s * GameConfig.PIXELS_TO_METERS);
+		createFixture(b, cs, CollisionLayers.CATEGORY_PICKUP, CollisionLayers.MASK_PICKUP, CollisionLayers.GROUP_PICKUP, 0, 0, 0, true);
+		return b;
+	}
+
+	public Body createShopBody(float x, float y, float w, float h) {
+		Body b = createRectBody(x, y, w * GameConfig.PIXELS_TO_METERS, h * GameConfig.PIXELS_TO_METERS,
+				CollisionLayers.CATEGORY_SHOP, CollisionLayers.MASK_SHOP, CollisionLayers.GROUP_SHOP, false, true);
+		b.setGravityScale(0);
+		return b;
+	}
+
+	public Body createPortalBody(float x, float y, int w, int h) {
+		Body b = createRectBody(x, y, w * GameConfig.PIXELS_TO_METERS, h * GameConfig.PIXELS_TO_METERS,
+				CollisionLayers.CATEGORY_PORTAL, CollisionLayers.MASK_PORTAL, CollisionLayers.GROUP_PORTAL, false, true);
+		b.setGravityScale(0);
+		return b;
+	}
+
+	public void Dispose() {
+		Array<Body> bodies = new Array<Body>();
+		this.world.getBodies(bodies);
+		for( Body b : bodies ) {
+			b.getWorld().destroyBody(b);
+		}
+	}
+
+
+}
