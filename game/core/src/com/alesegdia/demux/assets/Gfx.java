897e0f46531fd799fd259c807171f436fa0c56c2
Alejandro Seguí <alesegdia@gmail.com>
2016-01-03 22:00:00 +0100

    loading player assets

--- game/core/src/com/alesegdia/demux/assets/Gfx.java
+++ game/core/src/com/alesegdia/demux/assets/Gfx.java
@@ -1,6 +1,7 @@
 package com.alesegdia.demux.assets;
 
 import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.Animation.PlayMode;
 
 public class Gfx {
 
@@ -8,6 +9,16 @@ public class Gfx {
 	public static Animation playerStand;
 	public static Animation playerJumpUp;
 	public static Animation playerJumpDown;
-	public static Animation playerFly;
+	public static Spritesheet playerSheet;
 
+	public static void Initialize()
+	{
+		playerSheet = new Spritesheet("tiacascoanim.png", 3, 3);
+		playerStand = new Animation(0.2f, playerSheet.get(0));
+		playerJumpUp = new Animation(0.2f, playerSheet.get(1));
+		playerJumpDown = new Animation(0.2f, playerSheet.get(1));
+		playerWalk = new Animation(0.1f, playerSheet.getRange(2, 4));
+		playerWalk.setPlayMode(PlayMode.LOOP_PINGPONG);
+	}
+	
 }
