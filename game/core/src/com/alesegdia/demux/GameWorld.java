391b217da14ca260389f791e1b006f491c7ccc6a
Alejandro Seguí <alesegdia@gmail.com>
2016-01-04 22:16:26 +0100

    adding entrances to map

--- game/core/src/com/alesegdia/demux/GameWorld.java
+++ game/core/src/com/alesegdia/demux/GameWorld.java
@@ -1,6 +1,7 @@
 package com.alesegdia.demux;
 
 import com.alesegdia.demux.assets.Gfx;
+import com.alesegdia.demux.assets.TilemapWrapper;
 import com.alesegdia.demux.components.ActiveComponent;
 import com.alesegdia.demux.components.AnimationComponent;
 import com.alesegdia.demux.components.GraphicsComponent;
@@ -17,9 +18,14 @@ import com.alesegdia.demux.systems.FlipSystem;
 import com.alesegdia.demux.systems.HumanControllerSystem;
 import com.alesegdia.demux.systems.MovementSystem;
 import com.alesegdia.demux.systems.UpdatePhysicsSystem;
+import com.alesegdia.troidgen.room.Direction;
+import com.alesegdia.troidgen.room.Link;
+import com.alesegdia.troidgen.room.LinkInfo;
+import com.alesegdia.troidgen.room.Room;
 import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
 
 public class GameWorld {
 	
@@ -81,6 +87,54 @@ public class GameWorld {
 		engine.addEntity(player);
 	}
 	
+	public void makeEntrance(float x, float y, Direction dir)
+	{
+		Entity entrance = new Entity();
+		
+		TransformComponent tc = (TransformComponent) entrance.addComponent(new TransformComponent());
+		tc.position.set( x * 8, y * 8 );
+		tc.position.add( getOffsetForDirection(dir) );
+		
+		GraphicsComponent gc = (GraphicsComponent) entrance.addComponent(new GraphicsComponent());
+		gc.drawElement = Gfx.entranceSheet.get(0);
+		gc.sprite = new Sprite(gc.drawElement);
+		
+		AnimationComponent ac = (AnimationComponent) entrance.addComponent(new AnimationComponent());
+		ac.currentAnim = Gfx.entranceAnims[getIndexForDirection(dir)];
+		
+		engine.addEntity(entrance);
+	}
+	
+	private Vector2 getOffsetForDirection(Direction dir) {
+		Vector2 offset = new Vector2(0,0);
+		switch(dir)
+		{
+		case TOP:
+			offset.x = 4;
+			offset.y = 7.5f;
+			break;
+		case LEFT:
+			offset.x = 0.5f;
+			offset.y = 1;
+			break;
+		case RIGHT:
+			offset.x = 7.5f;
+			offset.y = 1;
+		}
+		return offset;
+	}
+
+	private int getIndexForDirection(Direction dir) {
+		switch(dir)
+		{
+		case TOP: return 2;
+		case DOWN: return 3;
+		case LEFT: return 1;
+		case RIGHT: return 0;
+		}
+		return -1;
+	}
+
 	public void setCam() {
 		cam.position.x = playerPositionComponent.position.x;
 		cam.position.y = playerPositionComponent.position.y;
@@ -105,5 +159,16 @@ public class GameWorld {
 	public boolean isPlayerDead() {
 		return player.isDead;
 	}
-	
+
+	public void buildMapEntities(TilemapWrapper currentMap) {
+		Room r = currentMap.createRoom();
+		for( Link l : r.links )
+		{
+			if( l.isConnected() )
+			{
+				makeEntrance(l.relCoord.x, l.relCoord.y, l.direction);
+			}
+		}
+	}
+
 }
