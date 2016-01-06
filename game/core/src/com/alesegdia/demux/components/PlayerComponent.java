cc52ddcbdc97534e4887e87f386012a920616e34
Alejandro Seguí <alesegdia@gmail.com>
2016-01-06 04:03:03 +0100

    adding some properties to player component

--- game/core/src/com/alesegdia/demux/components/PlayerComponent.java
+++ game/core/src/com/alesegdia/demux/components/PlayerComponent.java
@@ -1,11 +1,22 @@
 package com.alesegdia.demux.components;
 
 import com.alesegdia.demux.ecs.Component;
+import com.alesegdia.demux.ecs.Entity;
+import com.alesegdia.troidgen.room.Link;
+import com.alesegdia.troidgen.room.Room;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
 
 public class PlayerComponent extends Component{
 
 	public boolean jumping;
 	public boolean isPressingDown;
+	public boolean prevWasSolid;
+	public Link gotoRoom = null;
+	public Vector2 spawnPos;
+	public Entity linkEntity;
+	public boolean superJump;
+	public Body platform;
 	
 
 }
