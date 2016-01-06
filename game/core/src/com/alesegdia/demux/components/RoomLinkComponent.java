893eacbcf2a60b3f381f17de4345b81cacd288af
Alejandro Seguí <alesegdia@gmail.com>
2016-01-06 03:25:10 +0100

    adding room link component

---
+++ game/core/src/com/alesegdia/demux/components/RoomLinkComponent.java
@@ -0,0 +1,10 @@
+package com.alesegdia.demux.components;
+
+import com.alesegdia.demux.ecs.Component;
+import com.alesegdia.troidgen.room.Link;
+
+public class RoomLinkComponent extends Component {
+
+	public Link link;
+	
+}
