098b642193b2d499cf644532c3e0d4a9db5d3a02
Alejandro Seguí <alesegdia@gmail.com>
2016-01-06 14:10:31 +0100

    adding player respawn data

---
+++ game/core/src/com/alesegdia/demux/PlayerRespawnData.java
@@ -0,0 +1,14 @@
+package com.alesegdia.demux;
+
+import com.alesegdia.demux.components.PlayerComponent;
+import com.badlogic.gdx.math.Vector2;
+
+public class PlayerRespawnData {
+
+	public PlayerComponent plc = null;
+	public Vector2 prevlvc = null;
+	public Vector2 spawnPos = null;
+
+	
+	
+}
