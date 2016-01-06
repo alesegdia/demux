c1d7843f261faac926479d97e3eca129d690d71a
Alejandro Seguí <alesegdia@gmail.com>
2016-01-06 04:06:00 +0100

    changing control keys from arrows to A-D

--- game/core/src/com/alesegdia/demux/systems/HumanControllerSystem.java
+++ game/core/src/com/alesegdia/demux/systems/HumanControllerSystem.java
@@ -4,10 +4,12 @@ import com.alesegdia.demux.ecs.Entity;
 import com.alesegdia.demux.ecs.EntitySystem;
 import com.alesegdia.demux.assets.Gfx;
 import com.alesegdia.demux.components.AnimationComponent;
+import com.alesegdia.demux.components.DashComponent;
 import com.alesegdia.demux.components.GraphicsComponent;
 import com.alesegdia.demux.components.LinearVelocityComponent;
 import com.alesegdia.demux.components.PhysicsComponent;
 import com.alesegdia.demux.components.PlayerComponent;
+import com.alesegdia.demux.components.ShadowBufferEntry;
 import com.alesegdia.demux.components.TransformComponent;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
@@ -48,9 +50,9 @@ public class HumanControllerSystem extends EntitySystem {
 		int dx = 0; int dy = 0;
 		float prevYlinear = phc.body.getLinearVelocity().y;
 		
-		if(Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+		if(Gdx.input.isKeyPressed(Input.Keys.A)) {
 			dx = -1;
-		} else if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+		} else if(Gdx.input.isKeyPressed(Input.Keys.D)) {
 			dx = 1;
 		}
 
