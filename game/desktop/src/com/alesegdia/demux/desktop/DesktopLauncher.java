29272e80b00a7ae99d215a1fd46f22793a59633e
Alejandro Seguí <alesegdia@gmail.com>
2016-01-08 02:55:00 +0100

    using global config at desktop launcher

--- game/desktop/src/com/alesegdia/demux/desktop/DesktopLauncher.java
+++ game/desktop/src/com/alesegdia/demux/desktop/DesktopLauncher.java
@@ -2,11 +2,15 @@ package com.alesegdia.demux.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.alesegdia.demux.GameConfig;
 import com.alesegdia.demux.GdxGame;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = GameConfig.WINDOW_WIDTH;
+		config.height = GameConfig.WINDOW_HEIGHT;
+		config.title = "Demux";
 		new LwjglApplication(new GdxGame(), config);
 	}
 }
