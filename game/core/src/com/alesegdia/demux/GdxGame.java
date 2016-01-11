8f03e233c48b3c31e078490b95047e5378e7e3e4
Alejandro Seguí <alesegdia@gmail.com>
2016-01-11 20:55:22 +0100

    removing select map screen old stuff

--- game/core/src/com/alesegdia/demux/GdxGame.java
+++ game/core/src/com/alesegdia/demux/GdxGame.java
@@ -1,30 +1,10 @@
 package com.alesegdia.demux;
 
-import java.util.List;
-
 import com.alesegdia.demux.assets.Gfx;
 import com.alesegdia.demux.assets.Tmx;
-import com.alesegdia.demux.assets.TmxRoomInfoLoader;
 import com.alesegdia.demux.physics.Physics;
-import com.alesegdia.demux.screen.SelectMapScreen;
 import com.alesegdia.demux.screen.MapGameplayScreen;
 import com.alesegdia.demux.screen.RestartGameScreen;
-import com.alesegdia.troidgen.GraphBuilder;
-import com.alesegdia.troidgen.IRoomProvider;
-import com.alesegdia.troidgen.LayoutBuilder;
-import com.alesegdia.troidgen.LayoutBuilderConfig;
-import com.alesegdia.troidgen.LinkBuilder;
-import com.alesegdia.troidgen.ManualRoomProvider;
-import com.alesegdia.troidgen.MinSizeRoomGroupValidator;
-import com.alesegdia.troidgen.OverlapSolverConfig;
-import com.alesegdia.troidgen.RandomRoomProvider;
-import com.alesegdia.troidgen.renderer.RectDebugger;
-import com.alesegdia.troidgen.restriction.RestrictionSet;
-import com.alesegdia.troidgen.room.Room;
-import com.alesegdia.troidgen.room.RoomInfo;
-import com.alesegdia.troidgen.room.RoomType;
-import com.alesegdia.troidgen.util.Rect;
-import com.alesegdia.troidgen.util.UpperMatrix2D;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.OrthographicCamera;
@@ -36,7 +16,6 @@ public class GdxGame extends Game {
 	public ShapeRenderer srend;
 	public OrthographicCamera cam;
 	
-	public SelectMapScreen gameScreen;
 	public MapGameplayScreen tilemapScreen;
 	public Physics physics;
 	public RestartGameScreen restartScreen;
@@ -58,7 +37,6 @@ public class GdxGame extends Game {
         
 		physics = new Physics();
 
-        gameScreen = new SelectMapScreen(this);
         tilemapScreen = new MapGameplayScreen(this);
         restartScreen = new RestartGameScreen(this);
         setScreen(restartScreen);
