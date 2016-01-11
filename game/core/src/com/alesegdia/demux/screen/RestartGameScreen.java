9ab79e3252a2787aa8f23a190a475e3ebae0aecd
Alejandro Seguí <alesegdia@gmail.com>
2016-01-11 20:56:39 +0100

    setting spawn room as one in the biggest group of basic restricted rooms

--- game/core/src/com/alesegdia/demux/screen/RestartGameScreen.java
+++ game/core/src/com/alesegdia/demux/screen/RestartGameScreen.java
@@ -6,9 +6,18 @@ import java.util.List;
 import com.alesegdia.demux.GdxGame;
 import com.alesegdia.demux.map.MultipleConstraintComposer;
 import com.alesegdia.demux.map.SingleConstraintComposer;
+import com.alesegdia.troidgen.BiggestGroupFilter;
+import com.alesegdia.troidgen.GraphBuilder;
+import com.alesegdia.troidgen.GroupExtractor;
+import com.alesegdia.troidgen.IRoomGroupFilter;
 import com.alesegdia.troidgen.IWorldComposer;
+import com.alesegdia.troidgen.LinkBuilder;
+import com.alesegdia.troidgen.RoomRestrictionValidator;
 import com.alesegdia.troidgen.renderer.RectDebugger;
+import com.alesegdia.troidgen.restriction.RestrictionSet;
 import com.alesegdia.troidgen.room.Room;
+import com.alesegdia.troidgen.util.UpperMatrix2D;
+import com.alesegdia.troidgen.util.Util;
 import com.badlogic.gdx.Screen;
 
 public class RestartGameScreen implements Screen {
@@ -22,13 +31,31 @@ public class RestartGameScreen implements Screen {
 		this.g = gdxGame;
 	}
 
+	private List<List<Room>> computeStartRooms(List<Room> result) {
+		return new GroupExtractor().solve(result, new RoomRestrictionValidator(new RestrictionSet(4, false, false, false, false)));
+	}
+	
 	@Override
 	public void show() {
 
 		List<Room> result = composer.compose(new LinkedList<Room>());
 		this.roomLayout = result;
 		
-		Room r = result.get(0);
+		GraphBuilder gb = new GraphBuilder();
+		UpperMatrix2D<Float> m = gb.build(result);
+		
+		LinkBuilder linksb = new LinkBuilder();
+		linksb.generate(result);
+		
+		List<List<Room>> startRooms = computeStartRooms(result);
+
+		IRoomGroupFilter rgf = new BiggestGroupFilter();
+		
+		List<Room> start = rgf.filter(startRooms);
+		Util.shuffle(start);
+		
+		Room r = start.get(0);
+
         g.tilemapScreen.reset(r);
         g.setScreen(g.tilemapScreen);
 	}
