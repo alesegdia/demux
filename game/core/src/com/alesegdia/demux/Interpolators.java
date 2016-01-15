a6c866a1192e426df181d8c32d07c49d30180bd8
Alejandro Seguí <alesegdia@gmail.com>
2016-01-15 02:21:14 +0100

    tweaking stat interpolators

--- game/core/src/com/alesegdia/demux/Interpolators.java
+++ game/core/src/com/alesegdia/demux/Interpolators.java
@@ -1,16 +1,18 @@
 package com.alesegdia.demux;
 
-import com.alesegdia.troidgen.util.IInterpolator;
-import com.alesegdia.troidgen.util.Lerper;
+import com.alesegdia.demux.interp.Cubic;
+import com.alesegdia.demux.interp.Interpolator;
+import com.alesegdia.demux.interp.Lerper;
+import com.alesegdia.demux.interp.Square;
 
 public class Interpolators {
 
 	static private Interpolators instance = null;
 	
-	public IInterpolator speed = new Lerper(6f, 30f);
-	public IInterpolator ttl = new Lerper(0.2f, 1f);
-	public IInterpolator rate = new Lerper(0.01f, 1.f);
-	public IInterpolator power = new Lerper(2f, 10f);
+	public Interpolator speed = new Square(6f, 30f);
+	public Interpolator ttl = new Cubic(0.2f, 1f);
+	public Interpolator rate = new Square(0.01f, 1.f);
+	public Interpolator power = new Lerper(2f, 10f);
 	
 	public static Interpolators Instance()
 	{
