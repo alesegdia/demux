0a53d5a366672ff01e27519eed400b13d82b1b00
Alejandro Seguí <alesegdia@gmail.com>
2016-01-15 01:32:52 +0100

    updating bullet configs class

--- game/core/src/com/alesegdia/demux/BulletConfigs.java
+++ game/core/src/com/alesegdia/demux/BulletConfigs.java
@@ -3,6 +3,7 @@ package com.alesegdia.demux;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.alesegdia.troidgen.util.Lerper;
 import com.alesegdia.demux.assets.Gfx;
 import com.alesegdia.demux.components.ShootComponent.BulletEntry;
 import com.alesegdia.demux.components.ShootComponent.BulletModel;
@@ -49,62 +50,30 @@ public class BulletConfigs {
 	public static WeaponModel sacredPunch = new WeaponModel();
 	public static WeaponModel troqueGun = new WeaponModel();
 	
+	
 	public BulletConfigs() {
 
-		standardEnemyBM.h = 5;
-		standardEnemyBM.w = 5;
-		standardEnemyBM.tr = Gfx.playerBulletTexture;
-		standardEnemyBM.speed = 10;
-		standardEnemyBM.destructionTime = 1f;
-		standardEnemyBM.isPlayer = false;
-		standardEnemyBM.power = 1;
-		
-		standardEnemyDirectionalBM.h = 5;
-		standardEnemyDirectionalBM.w = 5;
-		standardEnemyDirectionalBM.tr = Gfx.playerBulletTexture;
-		standardEnemyDirectionalBM.speed = 10;
-		standardEnemyDirectionalBM.destructionTime = 1f;
-		standardEnemyDirectionalBM.isPlayer = false;
-		standardEnemyDirectionalBM.power = 1;
-		standardEnemyDirectionalBM.horizontal = false;
-		
-		createEntryInList(0,0, standardEnemyDirectionalBM, maskBEList);
-		
-		createEntryInList( -0.5f, -0.35f, standardEnemyBM, threeHeadedBEList );
-		createEntryInList( -0.5f, 0.3f, standardEnemyBM, threeHeadedBEList );
-		createEntryInList( -0.5f, 0.75f, standardEnemyBM, threeHeadedBEList );
-
-		/* RUNNER SHOOT CONFIG */
-		createEntryInList( 0, 0.3f, standardEnemyBM, runnerBEList );
-		
-		/* JUMPER SHOOT CONFIG */
-		createEntryInList( 0, 0.5f, standardEnemyBM, jumperBEList );
-		
-		/* DEMON SHOOT CONFIG */
-		createEntryInList( -0.3f, 0f, standardEnemyDirectionalBM, demonBEList );
-		createEntryInList( 0.3f, 0.3f, standardEnemyDirectionalBM, demonBEList );
-		
 		/* PLAYER DEFAULT SHOOT CONFIG */
 		playerDefaultBM.h = 5;
 		playerDefaultBM.w = 5;
 		playerDefaultBM.isPlayer = true;
-		playerDefaultBM.speed = 7;
+
+		playerDefaultBM.speed = Interpolators.Instance().speed.get(0.3f);
 		playerDefaultBM.tr = Gfx.playerBulletTexture;
-		playerDefaultBM.power = 2;
-		playerDefaultBM.destructionTime = 1f;
-		createEntryInList( -0.40f, 0.02f, playerDefaultBM, playerDefaultBEList );
-		defaultGun.rate = 0.2f;
+		playerDefaultBM.power = Interpolators.Instance().power.get(0.2f);
+		
+		playerDefaultBM.destructionTime = Interpolators.Instance().ttl.get(0.3f);
+
+		createEntryInList( -.40f, .02f, playerDefaultBM, playerDefaultBEList );
+		
+		defaultGun.rate = Interpolators.Instance().rate.get(0.2f);
 		defaultGun.bulletEntries = playerDefaultBEList;
 		//defaultGun.tr = Gfx.defaultSymbol;
-		
-		
+
 	}
 	
 	void createEntryInList( float oX, float oY, BulletModel bm, List<BulletEntry> belist ) {
-		BulletEntry be = new BulletEntry();
-		be.origin = new Vector2(oX, oY);
-		be.bm = bm;
-		belist.add(be);
+		belist.add(new BulletEntry(oX, oY, bm));
 	}
 	
 }
