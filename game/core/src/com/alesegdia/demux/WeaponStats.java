7b3dfb9f2e4c333c1fc59d3f8a62f3624ab5bdc9
Alejandro Seguí <alesegdia@gmail.com>
2016-01-15 01:33:23 +0100

    adding weapon stats helper class to perform upgrades

---
+++ game/core/src/com/alesegdia/demux/WeaponStats.java
@@ -0,0 +1,58 @@
+package com.alesegdia.demux;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import com.alesegdia.demux.assets.Gfx;
+import com.alesegdia.demux.components.ShootComponent.BulletEntry;
+import com.alesegdia.demux.components.ShootComponent.BulletModel;
+import com.alesegdia.demux.components.WeaponComponent.WeaponModel;
+import com.badlogic.gdx.math.Vector2;
+
+public class WeaponStats {
+
+	public int power;
+	public int rate;
+	public int ttl;
+	public int speed;
+	
+	// triple gun, sine gun, 5-gun
+	public int modifier;
+	
+	public WeaponStats(int p, int r, int t, int s) {
+		power = p;
+		rate = r;
+		ttl = t;
+		speed = s;
+	}
+
+	public WeaponModel makeModel()
+	{
+		WeaponModel wm = new WeaponModel();
+		wm.rate = Interpolators.Instance().rate.get((10-rate) * 0.1f);
+
+		// just for a single bullet by now
+		BulletEntry be = new BulletEntry();
+		be.origin = new Vector2(-.40f, .02f);
+		be.bm = new BulletModel();
+		
+		be.bm.destructionTime = Interpolators.Instance().ttl.get(ttl * 0.1f);
+		be.bm.h = 5;
+		be.bm.w = 5;
+		be.bm.tr = Gfx.playerBulletTexture;
+		be.bm.horizontal = true;
+		be.bm.isPlayer = true;
+		be.bm.trespassingEnabled = false;
+		be.bm.speed = Interpolators.Instance().speed.get(speed * 0.1f);
+		
+		List<BulletEntry> bes = new LinkedList<BulletEntry>();
+		bes.add(be);
+		
+		wm.ws = this;
+		
+		wm.bulletEntries = bes;
+
+		return wm;
+	}
+	
+}
