70a13b3f7f5828d97c81039de916231a060a416a
Alejandro Seguí <alesegdia@gmail.com>
2016-01-16 19:47:34 +0100

    using modifiers at weapon stats

--- game/core/src/com/alesegdia/demux/WeaponStats.java
+++ game/core/src/com/alesegdia/demux/WeaponStats.java
@@ -24,7 +24,7 @@ public class WeaponStats {
 	public int speed;
 	
 	// triple gun, sine gun, 5-gun
-	public int modifier;
+	public int modifier = 2;
 	
 	public WeaponStats(int p, int r, int t, int s) {
 		power = p;
@@ -32,17 +32,100 @@ public class WeaponStats {
 		ttl = t;
 		speed = s;
 	}
-
-	public WeaponModel makeModel()
+	
+	private List<BulletEntry> makeDefaultType()
 	{
-		WeaponModel wm = new WeaponModel();
-		wm.rate = Interpolators.Instance().rate.get((10-rate) * 0.1f);
-
 		// just for a single bullet by now
 		BulletEntry be = new BulletEntry();
 		be.origin = new Vector2(-.40f, .02f);
 		be.bm = new BulletModel();
 		
+		provideStats(be);
+		
+		List<BulletEntry> bes = new LinkedList<BulletEntry>();
+		bes.add(be);
+		return bes;		
+	}
+	
+	private List<BulletEntry> makeBinaryType()
+	{
+		List<BulletEntry> bes = new LinkedList<BulletEntry>();
+		
+		// just for a single bullet by now
+		BulletEntry be;
+		
+		be = new BulletEntry();
+		be.origin = new Vector2(-.40f, -.1f);
+		be.bm = new BulletModel();
+		provideStats(be);
+		bes.add(be);
+
+		be = new BulletEntry();
+		be.origin = new Vector2(-.40f, .1f);
+		be.bm = new BulletModel();
+		provideStats(be);
+		bes.add(be);
+
+		return bes;
+	}
+	
+	private List<BulletEntry> makeTriviumType()
+	{
+		List<BulletEntry> bes = new LinkedList<BulletEntry>();
+		
+		// just for a single bullet by now
+		BulletEntry be;
+		
+		be = new BulletEntry();
+		be.origin = new Vector2(-.40f, .02f);
+		be.bm = new BulletModel();
+		provideStats(be);
+		be.bm.dir = new Vector2(1,0.6f);
+		be.bm.horizontal = false;
+		bes.add(be);
+
+		be = new BulletEntry();
+		be.origin = new Vector2(-.40f, .02f);
+		be.bm = new BulletModel();
+		provideStats(be);
+		bes.add(be);
+
+		be = new BulletEntry();
+		be.origin = new Vector2(-.40f, .02f);
+		be.bm = new BulletModel();
+		provideStats(be);
+		be.bm.dir = new Vector2(1,-0.6f);
+		be.bm.horizontal = false;
+		bes.add(be);
+
+		return bes;
+	}
+	
+	private List<BulletEntry> makeWavegunType()
+	{
+		List<BulletEntry> bes = new LinkedList<BulletEntry>();
+		
+		// just for a single bullet by now
+		BulletEntry be;
+		be = new BulletEntry();
+		be.origin = new Vector2(-.40f, .02f);
+		be.bm = new BulletModel();
+		provideStats(be);
+		be.bm.sinegun = true;
+		bes.add(be);
+		
+		return bes;
+		
+	}
+	
+	private List<BulletEntry> makeDemuxType()
+	{
+		return null;
+		
+	}
+	
+	private void provideStats(BulletEntry be)
+	{
 		be.bm.destructionTime = Interpolators.Instance().ttl.get(ttl * 0.1f);
 		be.bm.h = 5;
 		be.bm.w = 5;
@@ -51,13 +134,23 @@ public class WeaponStats {
 		be.bm.isPlayer = true;
 		be.bm.trespassingEnabled = false;
 		be.bm.speed = Interpolators.Instance().speed.get(speed * 0.1f);
-		
-		List<BulletEntry> bes = new LinkedList<BulletEntry>();
-		bes.add(be);
-		
+	}
+
+	public WeaponModel makeModel()
+	{
+		WeaponModel wm = new WeaponModel();
+		wm.rate = Interpolators.Instance().rate.get((10-rate) * 0.1f);
+
 		wm.ws = this;
 		
-		wm.bulletEntries = bes;
+		switch( modifier )
+		{
+		case 0: wm.bulletEntries = makeDefaultType(); break;
+		case 1: wm.bulletEntries = makeBinaryType(); break;
+		case 2: wm.bulletEntries = makeTriviumType(); break;
+		case 3: wm.bulletEntries = makeWavegunType(); break;
+		case 4: wm.bulletEntries = makeDemuxType(); break;
+		}
 
 		return wm;
 	}
