6a811118bcc0099ec29aca0f06f5123f34d22d36
Alejandro Seguí <alesegdia@gmail.com>
2016-01-16 19:45:20 +0100

    adding upgrades component

---
+++ game/core/src/com/alesegdia/demux/components/UpgradesComponent.java
@@ -0,0 +1,18 @@
+package com.alesegdia.demux.components;
+
+import com.alesegdia.demux.ecs.Component;
+
+public class UpgradesComponent extends Component {
+
+	// weapon mods
+	public boolean hasBinary = false;
+	public boolean hasTrivium = false;
+	public boolean hasWavegun = false;
+	public boolean hasDemux = false;
+
+	// abilities
+	public boolean hasDash = false;
+	public boolean hasSJump = false;
+	
+	
+}
