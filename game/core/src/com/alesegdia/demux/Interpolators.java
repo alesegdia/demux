0de23e149a48ee10b014f06bf04dc9f6451c9ed6
Alejandro Seguí <alesegdia@gmail.com>
2016-01-16 19:44:22 +0100

    tweaking interpolators

--- game/core/src/com/alesegdia/demux/Interpolators.java
+++ game/core/src/com/alesegdia/demux/Interpolators.java
@@ -9,9 +9,9 @@ public class Interpolators {
 
 	static private Interpolators instance = null;
 	
-	public Interpolator speed = new Square(6f, 30f);
-	public Interpolator ttl = new Cubic(0.2f, 1f);
-	public Interpolator rate = new Square(0.01f, 1.f);
+	public Interpolator speed = new Lerper(6f, 30f);
+	public Interpolator ttl = new Lerper(0.2f, 0.5f);
+	public Interpolator rate = new Lerper(0.1f, .5f);
 	public Interpolator power = new Lerper(2f, 10f);
 	
 	public static Interpolators Instance()
