4156e406b423eb8e64a2183e31eebf464c73ee3f
Alejandro Seguí <alesegdia@gmail.com>
2016-01-16 19:47:15 +0100

    adding upgrades component to player respawn data

--- game/core/src/com/alesegdia/demux/PlayerRespawnData.java
+++ game/core/src/com/alesegdia/demux/PlayerRespawnData.java
@@ -5,6 +5,7 @@ import com.alesegdia.demux.components.GaugeComponent;
 import com.alesegdia.demux.components.PlayerComponent;
 import com.alesegdia.demux.components.ShootComponent;
 import com.alesegdia.demux.components.StaminaComponent;
+import com.alesegdia.demux.components.UpgradesComponent;
 import com.alesegdia.demux.components.WeaponComponent;
 import com.badlogic.gdx.math.Vector2;
 
@@ -18,5 +19,6 @@ public class PlayerRespawnData {
 	public WeaponComponent wc;
 	public GaugeComponent gc;
 	public StaminaComponent stc;
+	public UpgradesComponent uc;
 
 }
