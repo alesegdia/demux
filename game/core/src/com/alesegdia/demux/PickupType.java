2a93aa6d7c069400abc3ade67c989188d7c17a28
Alejandro Seguí <alesegdia@gmail.com>
2016-01-16 19:45:05 +0100

    adding static function to provide random pickup

--- game/core/src/com/alesegdia/demux/PickupType.java
+++ game/core/src/com/alesegdia/demux/PickupType.java
@@ -1,5 +1,29 @@
 package com.alesegdia.demux;
 
+import com.alesegdia.troidgen.util.RNG;
+
 public enum PickupType {
-	HEAL, INC_SP, INC_STAMINA, TRI_MOD, SINE_MOD, DEMUX_MOD
+	HEAL,
+	INC_SP, INC_STAMINA, INC_GAUGE,
+	ABILITY_DASH, ABILITY_SJUMP,
+	TRI_MOD, SINE_MOD, DEMUX_MOD, BI_MOD;
+	
+	public static PickupType MakeRandom()
+	{
+		PickupType pt = null;
+		switch(RNG.rng.nextInt(0,9))
+		{
+		case 0: pt = PickupType.ABILITY_DASH; break;
+		case 1: pt = PickupType.ABILITY_SJUMP; break;
+		case 2: pt = PickupType.BI_MOD; break;
+		case 3: pt = PickupType.DEMUX_MOD; break;
+		case 4: pt = PickupType.HEAL; break;
+		case 5: pt = PickupType.INC_GAUGE; break;
+		case 6: pt = PickupType.INC_SP; break;
+		case 7: pt = PickupType.INC_STAMINA; break;
+		case 8: pt = PickupType.SINE_MOD; break;
+		case 9: pt = PickupType.TRI_MOD; break;
+		}
+		return pt;
+	}
 }
