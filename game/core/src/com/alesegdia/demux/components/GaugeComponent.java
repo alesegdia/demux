db62e63149f3ca9fdc7c1a8afbad55d938eb777e
Alejandro Seguí <alesegdia@gmail.com>
2016-01-18 23:17:49 +0100

    tweaking gauge regen rate

--- game/core/src/com/alesegdia/demux/components/GaugeComponent.java
+++ game/core/src/com/alesegdia/demux/components/GaugeComponent.java
@@ -7,6 +7,7 @@ public class GaugeComponent extends Component {
 	public float maxGauge = 300;
 	public float currentGauge = 300;
 	public float regenRate = 1f;
+	//public float regenRate = 40f;
 	
 	public CharSequence gauge() {
 		return "" + Math.round(currentGauge * 100 / maxGauge) + "%";
